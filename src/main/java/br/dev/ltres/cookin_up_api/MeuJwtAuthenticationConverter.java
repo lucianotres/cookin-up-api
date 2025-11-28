f60f28cd8ba4455f84267489676f6701841ad37b
Luciano Tres <lucianotres@outlook.com>
2025-11-28 18:19:37 -0300

    Adicionado ciclo de autorização, porem não esta funcionando com o Entra ID, liberado por ora

---
+++ src/main/java/br/dev/ltres/cookin_up_api/MeuJwtAuthenticationConverter.java
@@ -0,0 +1,17 @@
+package br.dev.ltres.cookin_up_api;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+public class MeuJwtAuthenticationConverter extends JwtAuthenticationConverter {
+
+    public MeuJwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter delegate = new JwtGrantedAuthoritiesConverter();
+
+        delegate.setAuthorityPrefix("ROLE_");
+        delegate.setAuthoritiesClaimName("roles");
+
+        this.setJwtGrantedAuthoritiesConverter(delegate);
+    }
+}
